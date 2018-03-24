c07eb736f0cc38e4d5ab329bf5bf43ecb52aeb91
Marcus Jun <mjun@calstate.edu>
2018-03-24 16:35:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,9 +3,10 @@
 
 def display_board(board)
   x=0
-  puts " #{board[x]} | #{board[x++]} | #{board[x++]} "
+  puts " #{board[x]} | #{board[x+1]} | #{board[x+2]} "
   puts "-----------"
-  puts " #{board[x]} | #{board[x++]} | #{board[x++]} "
+  puts " #{board[x+3]} | #{board[x+4]} | #{board[x+5]} "
   puts "-----------"
-  puts " #{board[x]} | #{board[x++]} | #{board[x++]} "
+  puts " #{board[x+6]} | #{board[x+7]} | #{board[x+8]} "
 end
+
